7d653c95871028fec2d640d4af1143c484a60651
cilenedv <cilenedelacruz@gmail.com>
2016-11-10 20:13:49 -0500

    subiendo ejercicio

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+var opcion='<option value="{{#}}">{{name}}</option>';
+var opcion2='<div class="contenido">{{name}}</div>';
+$(document).ready(function(){
+	var especies="";
+	var personaje="";
+	$.getJSON("http://swapi.co/api/species/",function(response){
+            $.each(response.results, function(i,specie){
+            var id="";
+            var id2;
+            for(var i=0;i<specie.people.length;i++){
+            	id+=specie.people[i].substr(-3);
+            	id2=id.substring(0, id.length-1);
+            };
+            especies+=opcion.replace("{{name}}", specie.name).replace("{{#}}",id2);
+        });
+        $("#especies").change(function(e) {
+        	$("#contenido").html("");
+            var id3= $(this).val().split("/"); //
+            for (var i = 0; i < id3.length; i++) {
+                $.getJSON("http://swapi.co/api/people/" + id3[i] + "/", function(responce_2) {
+                        personaje=opcion2.replace("{{name}}", responce_2.name);
+                        $("#contenido").append(personaje);
+                    });
+            }
+        });
+        $("#especies").append(especies);
+    });
+});
+
